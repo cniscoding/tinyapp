b432a30434b3028b9c29229a657b1a1dccf56694
cniscoding <asdfa.com>
2023-02-21 16:50:32 -0800

    updated urls_new and added app.post(urls) to express_server

--- express_server.js
+++ express_server.js
@@ -4,14 +4,26 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/urls", (req,res) => {
   const templateVars = { urls : urlDatabase };
   res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
@@ -43,3 +55,5 @@ app.get("/fetch", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+// generateRandomString()
