cb5593fb33fe01e7db48dd82cf233b2ae5993e51
cniscoding <asdfa.com>
2023-02-21 21:03:04 -0800

    updated /u/:id to include res.resdirect(longURL). redirects are working

--- express_server.js
+++ express_server.js
@@ -21,17 +21,24 @@ const generateRandomString = function(uniqueLength){
   return randomString
 }
 
-// generateRandomString(6)
-
 app.post("/urls", (req, res) => {
-  const id = generateRandomString(6)
-  urlDatabase[id] = req.body.longURL;
-  res.redirect(`/urls/`)
-  // console.log(req.body); // Log the POST request body to the console
+  const id = generateRandomString(6) // creates a unique ID
+  urlDatabase[id] = req.body.longURL; // stores the newly created ID and long URL
+  res.redirect(`/urls/${id}`)
+  // console.log(req.body); // Log the POST request body to the console gives { longURL : 'address.com'}
+  // console.log(urlDatabase[id]) // gives 'address.com'
   // res.send("Ok"); // Respond with 'Ok' (we will replace this)
 });
 
-
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  console.log('longURL', longURL)
+  // console.log('urlDatabase', urlDatabase); // returns the urlDatabase object
+  // console.log(urlDatabase[req.params.id]) // www.yahoo.ca
+  // console.log('req.params', req.param) //req.params [Function: param]
+  // question - how come when i do https://www.yahoo.ca/ it works but when i go www.yahoo.ca it does not
+  res.redirect(longURL);
+});
 
 app.get("/urls", (req,res) => {
   const templateVars = { urls : urlDatabase };
@@ -47,26 +54,6 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/", (req, res) => {
-//   res.send("Hello!");
-// });
-
-// app.get("/url.json", (req, res)=>{
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b><body></html>\n");
-// });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
- 
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
